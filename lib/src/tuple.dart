8b72ed957cb73d7d5a2d08a5c422e07c027478b4
Kwang Yul Seo <kwangyul.seo@gmail.com>
2015-09-24 08:41:37 +0700

    Add Tuple6 and Tuple7 classes.

--- lib/src/tuple.dart
+++ lib/src/tuple.dart
@@ -220,3 +220,197 @@ class Tuple5<T1, T2, T3, T4, T5> {
         item5.hashCode
       ]);
 }
+
+/// Represents a 6-tuple, or sextuple.
+class Tuple6<T1, T2, T3, T4, T5, T6> {
+  /// Returns the first item of the tuple
+  final T1 item1;
+
+  /// Returns the second item of the tuple
+  final T2 item2;
+
+  /// Returns the third item of the tuple
+  final T3 item3;
+
+  /// Returns the fourth item of the tuple
+  final T4 item4;
+
+  /// Returns the fifth item of the tuple
+  final T5 item5;
+
+  /// Returns the sixth item of the tuple
+  final T6 item6;
+
+  /// Creates a new tuple value with the specified items.
+  const Tuple6(
+      this.item1, this.item2, this.item3, this.item4, this.item5, this.item6);
+
+  /// Returns a tuple with the first item set to the specified value.
+  Tuple6<T1, T2, T3, T4, T5, T6> withItem1(T1 v) {
+    return new Tuple6<T1, T2, T3, T4, T5, T6>(
+        v, item2, item3, item4, item5, item6);
+  }
+
+  /// Returns a tuple with the second item set to the specified value.
+  Tuple6<T1, T2, T3, T4, T5, T6> withItem2(T2 v) {
+    return new Tuple6<T1, T2, T3, T4, T5, T6>(
+        item1, v, item3, item4, item5, item6);
+  }
+
+  /// Returns a tuple with the third item set to the specified value.
+  Tuple6<T1, T2, T3, T4, T5, T6> withItem3(T3 v) {
+    return new Tuple6<T1, T2, T3, T4, T5, T6>(
+        item1, item2, v, item4, item5, item6);
+  }
+
+  /// Returns a tuple with the fourth item set to the specified value.
+  Tuple6<T1, T2, T3, T4, T5, T6> withItem4(T4 v) {
+    return new Tuple6<T1, T2, T3, T4, T5, T6>(
+        item1, item2, item3, v, item5, item6);
+  }
+
+  /// Returns a tuple with the fifth item set to the specified value.
+  Tuple6<T1, T2, T3, T4, T5, T6> withItem5(T5 v) {
+    return new Tuple6<T1, T2, T3, T4, T5, T6>(
+        item1, item2, item3, item4, v, item6);
+  }
+
+  /// Returns a tuple with the sixth item set to the specified value.
+  Tuple6<T1, T2, T3, T4, T5, T6> withItem6(T6 v) {
+    return new Tuple6<T1, T2, T3, T4, T5, T6>(
+        item1, item2, item3, item4, item5, v);
+  }
+
+  /// Creates a [List] containing the items of this [Tuple5].
+  ///
+  /// The elements are in item order. The list is variable-length
+  /// if [growable] is true.
+  List toList({bool growable: false}) =>
+      new List.from([item1, item2, item3, item4, item5, item6],
+          growable: growable);
+
+  @override
+  String toString() => '[$item1, $item2, $item3, $item4, $item5, $item6]';
+
+  @override
+  bool operator ==(o) => o is Tuple6 &&
+      o.item1 == item1 &&
+      o.item2 == item2 &&
+      o.item3 == item3 &&
+      o.item4 == item4 &&
+      o.item5 == item5 &&
+      o.item6 == item6;
+
+  @override
+  int get hashCode => _hash([
+        item1.hashCode,
+        item2.hashCode,
+        item3.hashCode,
+        item4.hashCode,
+        item5.hashCode,
+        item6.hashCode
+      ]);
+}
+
+/// Represents a 7-tuple, or septuple.
+class Tuple7<T1, T2, T3, T4, T5, T6, T7> {
+  /// Returns the first item of the tuple
+  final T1 item1;
+
+  /// Returns the second item of the tuple
+  final T2 item2;
+
+  /// Returns the third item of the tuple
+  final T3 item3;
+
+  /// Returns the fourth item of the tuple
+  final T4 item4;
+
+  /// Returns the fifth item of the tuple
+  final T5 item5;
+
+  /// Returns the sixth item of the tuple
+  final T6 item6;
+
+  /// Returns the seventh item of the tuple
+  final T7 item7;
+
+  /// Creates a new tuple value with the specified items.
+  const Tuple7(this.item1, this.item2, this.item3, this.item4, this.item5,
+      this.item6, this.item7);
+
+  /// Returns a tuple with the first item set to the specified value.
+  Tuple7<T1, T2, T3, T4, T5, T6, T7> withItem1(T1 v) {
+    return new Tuple7<T1, T2, T3, T4, T5, T6, T7>(
+        v, item2, item3, item4, item5, item6, item7);
+  }
+
+  /// Returns a tuple with the second item set to the specified value.
+  Tuple7<T1, T2, T3, T4, T5, T6, T7> withItem2(T2 v) {
+    return new Tuple7<T1, T2, T3, T4, T5, T6, T7>(
+        item1, v, item3, item4, item5, item6, item7);
+  }
+
+  /// Returns a tuple with the third item set to the specified value.
+  Tuple7<T1, T2, T3, T4, T5, T6, T7> withItem3(T3 v) {
+    return new Tuple7<T1, T2, T3, T4, T5, T6, T7>(
+        item1, item2, v, item4, item5, item6, item7);
+  }
+
+  /// Returns a tuple with the fourth item set to the specified value.
+  Tuple7<T1, T2, T3, T4, T5, T6, T7> withItem4(T4 v) {
+    return new Tuple7<T1, T2, T3, T4, T5, T6, T7>(
+        item1, item2, item3, v, item5, item6, item7);
+  }
+
+  /// Returns a tuple with the fifth item set to the specified value.
+  Tuple7<T1, T2, T3, T4, T5, T6, T7> withItem5(T5 v) {
+    return new Tuple7<T1, T2, T3, T4, T5, T6, T7>(
+        item1, item2, item3, item4, v, item6, item7);
+  }
+
+  /// Returns a tuple with the sixth item set to the specified value.
+  Tuple7<T1, T2, T3, T4, T5, T6, T7> withItem6(T6 v) {
+    return new Tuple7<T1, T2, T3, T4, T5, T6, T7>(
+        item1, item2, item3, item4, item5, v, item7);
+  }
+
+  /// Returns a tuple with the seventh item set to the specified value.
+  Tuple7<T1, T2, T3, T4, T5, T6, T7> withItem7(T7 v) {
+    return new Tuple7<T1, T2, T3, T4, T5, T6, T7>(
+        item1, item2, item3, item4, item5, item6, v);
+  }
+
+  /// Creates a [List] containing the items of this [Tuple5].
+  ///
+  /// The elements are in item order. The list is variable-length
+  /// if [growable] is true.
+  List toList({bool growable: false}) =>
+      new List.from([item1, item2, item3, item4, item5, item6, item7],
+          growable: growable);
+
+  @override
+  String toString() =>
+      '[$item1, $item2, $item3, $item4, $item5, $item6, $item7]';
+
+  @override
+  bool operator ==(o) => o is Tuple7 &&
+      o.item1 == item1 &&
+      o.item2 == item2 &&
+      o.item3 == item3 &&
+      o.item4 == item4 &&
+      o.item5 == item5 &&
+      o.item5 == item6 &&
+      o.item6 == item7;
+
+  @override
+  int get hashCode => _hash([
+        item1.hashCode,
+        item2.hashCode,
+        item3.hashCode,
+        item4.hashCode,
+        item5.hashCode,
+        item6.hashCode,
+        item7.hashCode
+      ]);
+}
